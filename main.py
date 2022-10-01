b0e0c7077d4654ea3075b747bf5cd0f0c635e163
Lucas Jun Koba Sato <lucasjks@gmail.com>
2022-10-01 00:38:43 -0700

    Complete basic experiment pipeline
    Now we can successfully run experiments and save their outputs.
    
    The remaining work should mostly be creating more experiments and
    analyzing the results, with minor refining of the pipeline itself.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import src.experiment_harness as harness
 
-__EXPERIMENT = None
+__EXPERIMENT_PATH = 'test.json'
+__API_KEY_PATH = 'data/api_key.txt'
 
 def main() -> None:
-    harness.run_experiment(__EXPERIMENT)
+    harness.run_experiment(__EXPERIMENT_PATH, __API_KEY_PATH)
 
 if __name__ == '__main__':
     main()
--- src/experiment_harness.py
+++ src/experiment_harness.py
@@ -1,7 +1,39 @@
 import os
+import json
 import openai
-import pandas as pd
 
-def run_experiment(exp_path: str) -> dict:
-    raise NotImplementedError
+def run_experiment(exp_path: str, api_key_path: str) -> dict:
+    api_key = get_api_key(api_key_path)
+    with open(exp_path, 'r') as f:
+        experiment = json.load(f)
+    results = {
+        'prompts': [],
+        'outputs': [],
+        'answers': experiment['answers'],
+    }
 
+    for prompt in experiment['prompts']:
+        full_prompt = experiment['prompt_prefix'] + prompt + experiment['prompt_postfix']
+        results['prompts'].append(full_prompt)
+        output = get_gpt3_output(full_prompt, api_key)
+        results['outputs'].append(output)
+
+    with open(f'data/results/{experiment["code"]}_results.json', 'w') as f:
+        json.dump(results, f, indent=4)
+
+    return results
+
+
+def get_api_key(api_key_path: str) -> str:
+    with open(api_key_path, 'r') as f:
+        return f.read().strip()
+
+
+def get_gpt3_output(prompt: str, api_key: str) -> str:
+    openai.api_key = api_key
+    return openai.Completion.create(
+        engine='text-davinci-002',
+        prompt=prompt,
+        temperature=0,
+        max_tokens=1
+    )['choices'][0]['text'].strip()
